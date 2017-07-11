8d2976462119a56dc755a2964d1a3f6b3b0a37a4
daniellund14 <daniel.lund14@gmail.com>
2017-07-11 03:00:31 -0700

    Changed install to move converter files

--- scripts/install_wat_wet_extractor.sh
+++ scripts/install_wat_wet_extractor.sh
@@ -2,8 +2,11 @@ git clone https://github.com/commoncrawl/ia-web-commons
 cd ia-web-commons 
 mvn -f pom-cdh5.xml install 
 # could also use pom.xml 
-cd - 
+cd -
+mv ia-web-commons $HOME
+
 
 git clone https://github.com/commoncrawl/ia-hadoop-tools 
 cd ia-hadoop-tools 
 mvn package
+mv ia-hadoop-tools $HOME
