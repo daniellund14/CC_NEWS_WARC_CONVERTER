151f8334a951c03947c1a544e18ef89e1c1ff89f
daniellund14 <daniel.lund14@gmail.com>
2017-07-11 02:32:00 -0700

    Updated download paths

--- s3_manager/driver.sh
+++ s3_manager/driver.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 PATHS_FILE='paths.txt'
 
-#bash warc_download_paths.sh $1 $PATHS_FILE
+bash warc_download_paths.sh $1 $PATHS_FILE
 bash download_warc.sh $PATHS_FILE
 bash convert_warc.sh
