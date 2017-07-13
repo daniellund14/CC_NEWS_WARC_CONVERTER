247d802e4a5419529e591f8625d28d16bdb06d26
daniellund14 <daniel.lund14@gmail.com>
2017-07-12 22:02:12 -0700

    added parse_newspaper

---
+++ scripts/driver.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+python parse_newspaper.py -f paths/paths_aa &
+python parse_newspaper.py -f paths/paths_ab &
+python parse_newspaper.py -f paths/paths_ac &
+python parse_newspaper.py -f paths/paths_ad &
+python parse_newspaper.py -f paths/paths_ae &
+python parse_newspaper.py -f paths/paths_af &
+python parse_newspaper.py -f paths/paths_ag &
+python parse_newspaper.py -f paths/paths_ah &
+python parse_newspaper.py -f paths/paths_ai &
+python parse_newspaper.py -f paths/paths_aj &
+python parse_newspaper.py -f paths/paths_ak &
+python parse_newspaper.py -f paths/paths_al &
+python parse_newspaper.py -f paths/paths_am &
+python parse_newspaper.py -f paths/paths_an &
+python parse_newspaper.py -f paths/paths_ao &
+python parse_newspaper.py -f paths/paths_ap &
+python parse_newspaper.py -f paths/paths_aq &
+python parse_newspaper.py -f paths/paths_ar &
+python parse_newspaper.py -f paths/paths_as &
+#paths_ab  paths_ac  paths_ad  paths_ae  paths_af  paths_ag  paths_ah  paths_ai  paths_aj  paths_ak  paths_al  paths_am  paths_an  paths_ao  paths_ap  paths_aq  paths_ar  paths_as
+wait
